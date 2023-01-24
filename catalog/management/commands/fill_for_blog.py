451be526d3a59d5c38dee418b3d691c86fd3f607
AytaDzhivanov1996 <kurt.alive.1994@gmail.com>
2023-01-24 19:57:16 +0300

    вроде сделал

---
+++ catalog/management/commands/fill_for_blog.py
@@ -0,0 +1,24 @@
+import datetime
+
+from django.core.management import BaseCommand
+import datetime
+from catalog.models import Blog
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        blogs = [
+            {'head': 'rambutan', 'slug': 'rambutan', 'content': 'hairy red', 'image': 'rambutan.jpg',
+             'creation_date': datetime.date.today()},
+            {'head': 'peaches', 'slug': 'peaches', 'content': 'hairy orange', 'image': 'peaches.jpg',
+             'creation_date': datetime.date.today()}
+        ]
+
+        blogs_list = []
+
+        for item in blogs:
+            blogs_list.append(Blog(**item))
+
+        Blog.objects.all().delete()
+        Blog.objects.bulk_create(blogs_list)
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products, pictures
+from catalog.views import home, contacts, ProductListView, pictures, BlogListView, BlogCreateView, BlogUpdateView, \
+    BlogDeleteView, BlogDetailView, change_status
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home),
     path('contacts/', contacts),
-    path('products/', products),
-    path('products/pictures/', pictures)
+    path('products/pictures/', pictures),
+    path('products/', ProductListView.as_view()),
+    path('products/blog/', BlogListView.as_view(), name='blog'),
+    path('products/blog/create/', BlogCreateView.as_view(), name='create'),
+    path('products/blog/update/<int:pk>/', BlogUpdateView.as_view(), name='update'),
+    path('products/blog/delete/<int:pk>/', BlogDeleteView.as_view(), name='delete'),
+    path('products/blog/detail/<int:pk>/', BlogDetailView.as_view(), name='detail'),
+    path('products/blog/staus/<int:pk>/', change_status, name='status')
 ]
